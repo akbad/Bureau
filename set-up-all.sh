4d27608510ac83487abb8a70bf5ffb2a16f720a0
Daniel Akbarzadeh <dakbarza@uwaterloo.ca>
2025-11-05 14:59:23 -0500

    Patch prompt files' symlink location bug

--- set-up-all.sh
+++ set-up-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-tools/scripts/set-up-tools.sh -y && \
 agents/scripts/set-up-agents.sh && \
 configs/scripts/set-up-configs.sh && \
+tools/scripts/set-up-tools.sh -y && \
 agents/scripts/set-up-claude-slash-commands.sh && \
 agents/scripts/set-up-codex-role-launchers.sh && \
 agents/scripts/set-up-gemini-role-launchers.sh
